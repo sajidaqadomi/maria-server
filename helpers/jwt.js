3e10f635bb5db6acbdb3c1938608bd78aa85ccff
Sajida Qadomi <sajidaqadomi@outlook.sa>
2022-03-13 01:53:16 +0200

    fix secret

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -11,6 +11,7 @@ export default function authJwt() {
         //  isRevoked: isRevoked
     }).unless({
         path: [
+            { url: /\/api\/v1\//, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: `${api}/carts`, methods: ['POST', 'OPTIONS'] },
